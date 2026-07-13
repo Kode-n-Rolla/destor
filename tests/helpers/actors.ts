d69e33c845546ffb5075fee36070346e2e24ade6
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-07-13 18:27:31 -0300

    add actors helper

---
+++ tests/helpers/actors.ts
@@ -0,0 +1,25 @@
+import * as anchor from "@coral-xyz/anchor";
+
+export const testAdmin = anchor.web3.Keypair.generate();
+
+export const airdrop = async (
+    provider: anchor.AnchorProvider,
+    pubkey: anchor.web3.PublicKey,
+    sol: number,
+) => {
+    const sig = await provider.connection.requestAirdrop(
+        pubkey,
+        sol * anchor.web3.LAMPORTS_PER_SOL
+    );
+
+    const latest = await provider.connection.getLatestBlockhash();
+
+    await provider.connection.confirmTransaction(
+        {
+            signature: sig,
+            blockhash: latest.blockhash,
+            lastValidBlockHeight: latest.lastValidBlockHeight
+        },
+        "confirmed"
+    );
+};
