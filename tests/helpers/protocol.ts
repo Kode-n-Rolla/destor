a04dc958986f2d57ea3c18e8d947d606d540283c
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-07-13 17:25:51 -0300

    add helper function for init protocol

---
+++ tests/helpers/protocol.ts
@@ -0,0 +1,39 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Destor } from "../../target/types/destor";
+import { program } from "@coral-xyz/anchor/dist/cjs/native/system";
+
+export const getProtocolPda = (programid: anchor.web3.PublicKey) => {
+    return anchor.web3.PublicKey.findProgramAddressSync(
+        [Buffer.from("protocol_config")],
+        programid
+    );
+};
+
+export const ensureProtocolInitialized = async (
+    program: Program<Destor>,
+    admin: anchor.web3.Keypair
+) => {
+    const [protocolPda, protocolBump] = getProtocolPda(program.programId);
+
+    const existingAccount = await program.provider.connection.getAccountInfo(protocolPda);
+
+    if (!existingAccount) {
+        await program.methods
+            .initialize()
+            .accountsPartial({
+                admin: admin.publicKey,
+                protocolConfig: protocolPda
+            })
+            .signers([admin])
+            .rpc();
+    }
+
+    const protocolConfig = await program.account.protocolConfig.fetch(protocolPda);
+
+    return {
+        protocolPda,
+        protocolBump,
+        protocolConfig
+    };
+};
