f3e7623a29c5ab565bfc52739243182aaa0cb289
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-07-13 18:23:27 -0300

    add role helper

---
+++ tests/helpers/roles.ts
@@ -0,0 +1,7 @@
+export const Roles = {
+    manufacturer: { manufacturer: {} },
+    service: { service: {} },
+    roadInspection: { roadInspection: {} },
+    insurance: { insurance: {} },
+    owner: { owner: {} },
+} as const;
