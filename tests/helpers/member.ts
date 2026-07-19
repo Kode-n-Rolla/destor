c3679bc4bd5ca529a1b2e38478716cb866492ace
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-07-19 11:32:03 -0300

    add helper for member creation

--- tests/helpers/member.ts
+++ tests/helpers/member.ts
@@ -1,4 +1,13 @@
 import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Destor } from "../../target/types/destor";
+
+type CreateMemberArgs = {
+    program: Program<Destor>;
+    organizationPda: anchor.web3.PublicKey;
+    authority: anchor.web3.Keypair;
+    wallet?: anchor.web3.Keypair;
+};
 
 export const getMemberPda = (
     programId: anchor.web3.PublicKey,
@@ -13,4 +22,36 @@ export const getMemberPda = (
         ],
         programId
     );
+};
+
+export const createMember = async ({
+    program,
+    organizationPda,
+    authority,
+    wallet = anchor.web3.Keypair.generate(),
+}: CreateMemberArgs) => {
+    const [memberPda, memberBump] = getMemberPda(
+        program.programId,
+        organizationPda,
+        wallet.publicKey
+    );
+
+    await program.methods
+        .addOrganizationMember(wallet.publicKey)
+        .accountsPartial({
+            authority: authority.publicKey,
+            member: memberPda,
+            organization: organizationPda
+        })
+        .signers([authority])
+        .rpc();
+    
+    const memberAccount = await program.account.member.fetch(memberPda);
+
+    return {
+        wallet,
+        memberPda,
+        memberBump,
+        memberAccount
+    };
 };
