eeecf0ce798141641a18fdb1a6a1c9d65706ccd6
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-07-29 17:35:20 -0300

    add constants

--- programs/destor/src/constant.rs
+++ programs/destor/src/constant.rs
@@ -2,5 +2,11 @@ pub const PROTOCOL_CONFIG_SEED: &[u8] = b"protocol_config";
 pub const ORGANIZATION_SEED: &[u8] = b"organization";
 pub const MEMBER_SEED: &[u8] = b"member";
 pub const VEHICLE_SEED: &[u8] = b"vehicle";
+pub const NOTE_SEED: &[u8] = b"note";
+
 pub const MAX_COLOR_LENGTH: usize = 30;
 pub const MAX_MODEL_LENGTH: usize = 30;
+pub const MAX_DESCRIPTION_LENGTH: usize = 200;
+pub const MAX_REPORT_URI_LENGTH: usize = 120;
+pub const MAX_REPORT_HASH_LENGTH: usize = 64;
+pub const MAX_NOTE_SIGNERS: usize = 8;
