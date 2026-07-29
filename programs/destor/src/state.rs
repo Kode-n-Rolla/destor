0a9951b21a4a1cb990023b96b827334d841152c5
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-07-29 17:34:55 -0300

    clarify INIT_SPACEs and add for Note

--- programs/destor/src/state.rs
+++ programs/destor/src/state.rs
@@ -1,4 +1,4 @@
-use crate::constant::{MAX_COLOR_LENGTH, MAX_MODEL_LENGTH};
+use crate::constant::{MAX_COLOR_LENGTH, MAX_DESCRIPTION_LENGTH, MAX_MODEL_LENGTH, MAX_NOTE_SIGNERS, MAX_REPORT_HASH_LENGTH, MAX_REPORT_URI_LENGTH};
 
 use super::types::*;
 use anchor_lang::prelude::*;
@@ -10,7 +10,10 @@ pub struct ProtocolConfig {
 }
 
 impl ProtocolConfig {
-    pub const INIT_SPACE: usize = 8 + 32 + 1;
+    pub const INIT_SPACE: usize =
+        8     // discriminator
+        + 32  // admin
+        + 1;  // bump
 }
 
 #[account]
@@ -23,14 +26,25 @@ pub struct Vehicle {
     pub owner: Pubkey,
     pub color: String,
     pub mileage: u64,     // is the latest accepted mileage.
-    pub note_count: u64,  // is used to derive Note PDA addresses.
+    pub next_note_index: u64,  // is used to derive Note PDA addresses.
     pub owner_count: u16, // increments on ownership transfer.
     pub bump: u8,
 }
 
 impl Vehicle {
     pub const INIT_SPACE: usize =
-        8 + 32 + 32 + 32 + (4 + MAX_MODEL_LENGTH) + 8 + 32 + (4 + MAX_COLOR_LENGTH) + 8 + 8 + 2 + 1;
+        8       // discriminator
+        + 32    // vin_hash
+        + 32    // nft_asset
+        + 32    // manufacturer
+        + (4 + MAX_MODEL_LENGTH) // model
+        + 8     // manufacturer_at
+        + 32    // owner
+        + (4 + MAX_COLOR_LENGTH) //color
+        + 8     // mileage
+        + 8     // next_note_index
+        + 2     // onwer_count
+        + 1;    // bump
 }
 
 #[account]
@@ -45,7 +59,15 @@ pub struct Organization {
 }
 
 impl Organization {
-    pub const INIT_SPACE: usize = 8 + 1 + 32 + 32 + 32 + 1 + 1 + 1;
+    pub const INIT_SPACE: usize =
+        8     // discriminator
+        + 1   // role
+        + 32  // authority
+        + 32  // pending_authority
+        + 32  // organization_id
+        + 1   // threshold
+        + 1   // active
+        + 1;  // bump
 }
 
 // could be official service, dealer, minter. @todo think about add enum MemberRole
@@ -58,7 +80,12 @@ pub struct Member {
 }
 
 impl Member {
-    pub const INIT_SPACE: usize = 8 + 32 + 32 + 1 + 1;
+    pub const INIT_SPACE: usize =
+        8     // discriminator
+        + 32  // organization
+        + 32  // waller
+        + 1   // active
+        + 1;  // bump
 }
 
 #[account]
@@ -75,4 +102,21 @@ pub struct Note {
     pub report_uri: String,  // should have a max length.
     pub report_hash: String, // can verify the off-chain report content.
     pub bump: u8,
+}
+
+impl Note {
+    pub const INIT_SPACE: usize =
+        8     // discriminator
+        + 1   // status
+        + 32  // vehicle
+        + 8   // note_index
+        + 1   // role
+        + 1   // note_kind
+        + 8   // mileage
+        + 8   // timestamp
+        + (4 + MAX_DESCRIPTION_LENGTH) // description
+        + (4 + MAX_NOTE_SIGNERS * 32)  // signers
+        + (4 + MAX_REPORT_URI_LENGTH)  // report_uri
+        + (4 + MAX_REPORT_HASH_LENGTH) // report_hash
+        +1;   // bump 
 }
