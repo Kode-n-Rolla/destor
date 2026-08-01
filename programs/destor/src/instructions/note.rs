16054f2db55b8c0532cd22c373d5451aa853b9f3
Kode-n-Rolla <pavlucio.auditore@gmail.com>
2026-08-01 11:44:33 -0300

    add: note.rs with add organization note functionality

---
+++ programs/destor/src/instructions/note.rs
@@ -0,0 +1,135 @@
+use anchor_lang::prelude::*;
+
+use crate::{
+    constant::{INSURANCE_NOTE_SIGNERS, MANUFACTURER_NOTE_SIGNERS, MAX_DESCRIPTION_LENGTH, MAX_REPORT_HASH_LENGTH, MAX_REPORT_URI_LENGTH, MEMBER_SEED, NOTE_SEED, ORGANIZATION_SEED, OWNER_NOTE_SIGNERS, ROAD_INSPECTION_NOTE_SIGNERS, SERVICE_HUB_NOTE_SIGNERS, VEHICLE_SEED}, error::DeStorError, events::AddedNote, state::{Member, Note, Organization, Vehicle}, types::{
+        NoteKind,
+        Role,
+        Status,
+    },
+};
+
+#[derive(Accounts)]
+#[instruction(vin_hash: [u8; 32])]
+pub struct AddOrganizationNote<'info> {
+    #[account(mut)]
+    pub wallet: Signer<'info>,
+
+    #[account(
+        seeds = [ORGANIZATION_SEED, organization.organization_id.as_ref()],
+        bump,
+    )]
+    pub organization: Account<'info, Organization>,
+
+    #[account(
+        seeds = [MEMBER_SEED, organization.key().as_ref(), wallet.key().as_ref()],
+        bump,
+        has_one = wallet,
+    )]
+    pub member: Account<'info, Member>,
+
+    #[account(
+        init,
+        payer = wallet,
+        space = Note::INIT_SPACE,
+        seeds = [NOTE_SEED, vehicle.key().as_ref(), &vehicle.next_note_index.to_le_bytes()],
+        bump
+    )]
+    pub note: Account<'info, Note>,
+
+    #[account(
+        mut,
+        seeds = [VEHICLE_SEED, vin_hash.as_ref()],
+        bump,
+    )]
+    pub vehicle: Account<'info, Vehicle>,
+
+    pub system_program: Program<'info, System>, 
+}
+
+pub fn add_organization_note(
+    ctx: Context<AddOrganizationNote>,
+    vin_hash: [u8; 32],
+    description: String,
+    mileage: u64,
+    report_uri: String,
+    report_hash: String,
+) -> Result<()> {
+    require_eq!(ctx.accounts.organization.key(), ctx.accounts.member.organization, DeStorError::InvalidMember);
+    if ctx.accounts.vehicle.vin_hash != vin_hash {
+        return err!(DeStorError::InvalidVin);
+    }
+    require!(ctx.accounts.organization.active, DeStorError::OrganizationNotActive);
+    require!(ctx.accounts.member.active, DeStorError::MemberIsNotActive);
+    require_gte!(MAX_DESCRIPTION_LENGTH, description.len(),DeStorError::DescriptionToLong);
+    require_gte!(MAX_REPORT_URI_LENGTH, report_uri.len(), DeStorError::ReportUriToLong);
+    require_gte!(MAX_REPORT_HASH_LENGTH, report_hash.len(), DeStorError::ReportHashToLong);
+
+    let vehicle = &mut ctx.accounts.vehicle;
+    let note = &mut ctx.accounts.note;
+    let current_time = Clock::get()?.unix_timestamp;
+    let required_signers = match ctx.accounts.organization.role {
+        Role::Manufacturer => MANUFACTURER_NOTE_SIGNERS,
+        Role::ServiceHub => SERVICE_HUB_NOTE_SIGNERS,
+        Role::RoadInspection => ROAD_INSPECTION_NOTE_SIGNERS,
+        Role::Insurance => INSURANCE_NOTE_SIGNERS,
+        Role::Owner => OWNER_NOTE_SIGNERS,
+    };
+
+    note.status = Status::Pending;
+    note.vehicle = vehicle.key();
+    note.note_index = vehicle.next_note_index;
+    note.role = ctx.accounts.organization.role;
+    note.note_kind = match ctx.accounts.organization.role {
+        Role::Manufacturer => NoteKind::Manufacturing,
+        Role::ServiceHub => NoteKind::Service,
+        Role::RoadInspection => NoteKind::Accident,
+        Role::Insurance => NoteKind::InsuranceReport,
+        Role::Owner => NoteKind::OwnerMaintenance,
+    };
+    note.mileage = mileage;
+    note.timestamp = current_time;
+    note.description = description;
+
+    note.signers = Vec::with_capacity(required_signers);
+    note.signers.push(ctx.accounts.wallet.key());
+
+    note.report_uri = report_uri;
+    note.report_hash = report_hash;
+    note.bump = ctx.bumps.note;
+
+    vehicle.next_note_index += 1;
+
+    emit!(AddedNote {
+        signer: ctx.accounts.wallet.key(),
+        vehicle_pda: vehicle.key(),
+        note_pda: note.key(),
+        note_index: note.note_index,
+        role: ctx.accounts.organization.role,
+        note_kind: note.note_kind,
+        timestamp: current_time,
+    });
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct AddOwnerNote {
+
+}
+
+pub fn add_owner_note() -> Result<()> {
+
+    // @todo add event and handler to lib.rs
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct SignNote {
+    
+}
+
+pub fn sign_note() -> Result<()> {
+
+    // @todo add event and handler to lib.rs
+    Ok(())
+}
